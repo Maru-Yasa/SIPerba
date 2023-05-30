42d5213c4727d733eecfe52e0667ffc080971667
Maru-Yasa <marufilyasa13@gmail.com>
2023-05-30 11:56:06 +0700

    fix fitur print

--- application/views/home/detailHistory.php
+++ application/views/home/detailHistory.php
@@ -14,26 +14,43 @@
             <img src="/assets/images/undraw_file_manager_re_ms29.svg" width="200" alt="">
         </div>
     </div>
+	<button class="btn btn-primary gap-2 mt-3" onclick="windowPrint()"><i class="bi bi-printer-fill"></i> Print</button>
 </div>
 
 
 <div id="printable" class="w-full px-12 mt-10">
+
+	<div class="mb-5 text-center flex ">
+		<img src="/assets/images/logo.png" class="w-32" alt="">
+		<div class="align-baseline">
+			<h1 class="text-xl font-bold">
+				PT. WIJAYA CONSTRUCTION
+				Contractor & Building Solution
+			</h1>
+			<h3 class="text-md font-bold">
+						Jl. Dukuh Kupang XXV No.54, Dukuh Kupang, Kec.Dukuhpakis, Surabaya, Jawa Timur 60225
+				Email : info@uwks.ac.id; Telp : (031) 5677577; Whatsapp : 082131006331
+			</h3>
+		</div>
+	</div>
+	<hr class="h-1 my-8 bg-black">
+
 	<div class="mb-5">
 		<h1 class="text-2xl font-bold">Input Perhitungan</h1>
 		<table class="table w-full rounded-lg bg-primary">
 			<thead>
-				<td class="normal-case">$(b)$ Lebar Balok (inci)</td>
-				<td class="normal-case">$(d)$ Tinggi Efektif Balok (inci)</td>
-				<td class="normal-case">$(As)$ Luas Tulangan Tarik (inci$,^2$)</td>
-				<td class="normal-case">$(f'c)$ Kekuatan Mutu Beton (psi)</td>
-				<td class="normal-case">$(F_y)$ Mutu Baja (psi)</td>
+				<td class="normal-case border">$(b)$ Lebar Balok (inci)</td>
+				<td class="normal-case border">$(d)$ Tinggi Efektif Balok (inci)</td>
+				<td class="normal-case border">$(As)$ Luas Tulangan Tarik (inci$,^2$)</td>
+				<td class="normal-case border">$(f'c)$ Kekuatan Mutu Beton (psi)</td>
+				<td class="normal-case border">$(F_y)$ Mutu Baja (psi)</td>
 			</thead>
 			<tbody class="">
-				<td class=""><?= $history['b'] ?></td>
-				<td class=""><?= $history['d'] ?></td>
-				<td class=""><?= $history['as2'] ?></td>
-				<td class=""><?= $history['fc'] ?></td>
-				<td class=""><?= $history['fy'] ?></td>
+				<td class="border"><?= $history['b'] ?></td>
+				<td class="border"><?= $history['d'] ?></td>
+				<td class="border"><?= $history['as2'] ?></td>
+				<td class="border"><?= $history['fc'] ?></td>
+				<td class="border"><?= $history['fy'] ?></td>
 			</tbody>
 		</table>
 	</div>
@@ -41,48 +58,34 @@
 		<h1 class="text-2xl font-bold">Data Rumus</h1>
 		<table class="table w-full rounded-lg bg-primary">
 			<thead>
-				<td class="normal-case">$(a)$</td>
-				<td class="normal-case">$(beta)$</td>
-				<td class="normal-case">$(c)$</td>
-				<td class="normal-case">$(C)$</td>
-				<td class="normal-case">$(T)$</td>
-				<td class="normal-case">$(cdt)$</td>
-				<td class="normal-case">$(et)$</td>
-				<td class="normal-case">$(Pmin)$</td>
-				<td class="normal-case">$(P)$</td>
+				<td class="normal-case border">$(a)$</td>
+				<td class="normal-case border">$(beta)$</td>
+				<td class="normal-case border">$(c)$</td>
+				<td class="normal-case border">$(C)$</td>
+				<td class="normal-case border">$(T)$</td>
+				<td class="normal-case border">$(cdt)$</td>
+				<td class="normal-case border">$(et)$</td>
+				<td class="normal-case border">$(Pmin)$</td>
+				<td class="normal-case border">$(P)$</td>
 			</thead>
 			<tbody class="">
-				<td class=""><?= $rumus['input']['a'] ?></td>
-				<td class=""><?= $rumus['input']['beta'] ?></td>
-				<td class=""><?= $rumus['input']['c'] ?></td>
-				<td class=""><?= $rumus['input']['C'] ?></td>
-				<td class=""><?= $rumus['input']['T'] ?></td>
-				<td class=""><?= isset($rumus['input']['cdt']) ? $rumus['input']['cdt'] : '-'?></td>
-				<td class=""><?= isset($rumus['input']['et']) ? $rumus['input']['et'] : '-'?></td>
-				<td class=""><?= $rumus['input']['pMin1'] ?></td>
-				<td class=""><?= $rumus['input']['p'] ?></td>
+				<td class="border"><?= $rumus['input']['a'] ?></td>
+				<td class="border"><?= $rumus['input']['beta'] ?></td>
+				<td class="border"><?= $rumus['input']['c'] ?></td>
+				<td class="border"><?= $rumus['input']['C'] ?></td>
+				<td class="border"><?= $rumus['input']['T'] ?></td>
+				<td class="border"><?= isset($rumus['input']['cdt']) ? $rumus['input']['cdt'] : '-'?></td>
+				<td class="border"><?= isset($rumus['input']['et']) ? $rumus['input']['et'] : '-'?></td>
+				<td class="border"><?= $rumus['input']['pMin1'] ?></td>
+				<td class="border"><?= $rumus['input']['p'] ?></td>
 			</tbody>
 		</table>
 	</div>
 	<div class="mb-5">
 		<h1 class="text-2xl font-bold">Perhitungan</h1>
-		<p id="rumus">
+		<ul id="rumus" class="list-disc">
 
-		</p>
-	</div>
-	<div class="mb-5">
-		<h1 class="text-2xl font-bold mb-3">Data Hasil</h1>
-		<p>
-			<?php if ($rumus['status'] == false) { ?>
-				<span class="text-error">
-					<?= $rumus['data'] ?>
-				</span>
-			<?php } else { ?>
-				<span class="">
-					$M_n = $ <?= $rumus['data'] ?>
-				</span>
-			<?php } ?>
-		</p>
+		</ul>
 	</div>
 </div>
 
@@ -104,6 +107,15 @@
 		}
 	};
 
+	function windowPrint() {
+		var printable = document.getElementById('printable')
+		var originalContent = document.body.innerHTML
+
+		document.body.innerHTML = printable.innerHTML
+		window.print()
+		document.body.innerHTML = originalContent
+	}
+
 	function printDoc() {
 		var doc = new window.jsPDF('l', 'pt', 'letter')
 		var printable = document.getElementById('printable')
@@ -120,48 +132,61 @@
 		})
 	}
 
+	function doPdf() {
+		var pdf = new jsPDF('l', 'pt', 'letter');
+		var printable = document.getElementById('printable')
+		pdf.html(printable, {
+			callback: function (pdf) {
+				var iframe = document.createElement('iframe');
+				iframe.setAttribute('style', 'position:absolute;right:0; top:0; bottom:0; height:100%; width:500px');
+				document.body.appendChild(iframe);
+				iframe.src = pdf.output('datauristring');
+			}
+		});
+	}
+
 	function visualMn(as, fy, d, a) {
-			return `$M_n = A_s xx f_y (d - a / 2) = ${(as)} xx ${fy} (${d} - ${a} / 2)$`
+			return `<li> $M_n = A_s xx f_y (d - a / 2) = ${(as)} xx ${fy} (${d} - ${a} / 2)$ </li>`
 		}
 
 		function visualA(as, fy, fc, b, a) {
-			return `$a = (A_s xx f_y) / (0.85 xx f'c xx b) = (${as} xx ${fy}) / (0.85 xx ${fc} xx ${b}) = ${a}$ inci`
+			return `<li>$a = (A_s xx f_y) / (0.85 xx f'c xx b) = (${as} xx ${fy}) / (0.85 xx ${fc} xx ${b}) = ${a}$ inci</li>`
 		}
 
 		function visualEt(d, c, et) {
-			return `$ε_t = 0,003((d_t-c) / c)$ <br> $ε_t = 0,003((${d}-${c}) / ${c}) = ${et} > 0.005$`
+			return `<li>$ε_t = 0,003((d_t-c) / c)$ <br> $ε_t = 0,003((${d}-${c}) / ${c}) = ${et} > 0.005$</li>`
 		}
 
 		function visualCdt(c, d, cdt, terkontrolTekan = false) {
-			return `$c/d_t = ${c} / ${d} = ${cdt}$ ${terkontrolTekan == true ? '(Terkontrol Tekan)' : ''}`
+			return `<li>$c/d_t = ${c} / ${d} = ${cdt}$ ${terkontrolTekan == true ? '(Terkontrol Tekan)' : ''}</li>`
 		}
 
 		function visualC(a, beta, c) {
-			return `$c = a/β_1 = ${a}/${beta} = ${c}$ inci`
+			return `<li>$c = a/β_1 = ${a}/${beta} = ${c}$ inci</li>`
 		}
 
 		function visualBeta(beta) {
-			return `$β_1 = ${beta}$`
+			return `<li>$β_1 = ${beta}$</li>`
 		}
 
 		function visualRo(p, as, b, d) {
-			return `$ρ = A_s / (b xx d) = ${as} / (${b} xx ${d}) = ${p}$`
+			return `<li>$ρ = A_s / (b xx d) = ${as} / (${b} xx ${d}) = ${p}$</li>`
 		}
 
 		function visualRoMin(pMin, fc, fy) {
-			return `$ρ_min = root(3)(f'c)/(fy) = root(3)(${fc})/${fy}$ = ${(pMin)}`
+			return `<li>$ρ_min = root(3)(f'c)/(fy) = root(3)(${fc})/${fy}$ = ${(pMin)}</li>`
 		}
 
 		function visualCBesar(fc, b, a, C) {
-			return `$C = 0.85 xx f'c xx b xx a = 0.85 xx ${fc} xx ${b} xx ${a} = ${C}a $ $lb$`
+			return `<li>$C = 0.85 xx f'c xx b xx a = 0.85 xx ${fc} xx ${b} xx ${a} = ${C}a $ $lb$</li>`
 		}
 
 		function visualT(T, as, fy) {
-			return `$T = A_s xx f_y = ${as} xx ${fy} = ${T} lb$`;
+			return `$T = A_s xx f_y = ${as} xx ${fy} = ${T} lb$</li>`;
 		}
 
 		function visualDt(d) {
-			return `$d_t = d = ${d}$ inci`;
+			return `$d_t = d = ${d}$ inci</li>`;
 		}
 
 
@@ -184,7 +209,7 @@
 		hasilSpan.classList.remove('text-error')
 		hasilSpan.classList.add('text-inherit')
 		hasilSpan.innerHTML =
-			`	$f'c = ${input['fc']}$ psi <br>
+			`	<li> $f'c = ${input['fc']}$ psi </li> <br>
 				${visualRoMin(data.input['pMin1'], input['fc'], input['fy'])} <br>
 				${visualRo(data.input['p'], input['as'], input['b'], input['d'])} <br>
 				${data.input['perbandinganRo']} <br>
@@ -206,7 +231,7 @@
 		if (data.input.stepError == 1) {
 
 			hasilSpan.innerHTML = `
-				$f'c = ${input['fc']}$ psi <br>
+				<li> $f'c = ${input['fc']}$ psi </li> <br>
 				${visualRoMin(data.input['pMin1'], input['fc'], input['fy'])} <br>
 				${visualRo(data.input['p'], input['as'], input['b'], input['d'])} <br>
 				<span class="text-error">${data.data}</span>
@@ -216,7 +241,7 @@
 		if (data.input.stepError == 2) {
 			// c/dt yang error
 			hasilSpan.innerHTML = `
-				$f'c = ${input['fc']}$ psi <br>
+				<li> $f'c = ${input['fc']}$ psi </li> <br>
 				${visualRoMin(data.input['pMin1'], input['fc'], input['fy'])} <br>
 				${visualRo(data.input['p'], input['as'], input['b'], input['d'])} <br>
 				${data.input['perbandinganRo']} <br>
@@ -235,7 +260,7 @@
 		if (data.input.stepError == 3) {
 			// et yang error
 			hasilSpan.innerHTML = `
-			$f'c = ${input['fc']}$ psi <br>
+			<li> $f'c = ${input['fc']}$ psi </li><br>
 			${visualRoMin(data.input['pMin1'], input['fc'], input['fy'])} <br>
 			${visualRo(data.input['p'], input['as'], input['b'], input['d'])} <br>
 			${data.input['perbandinganRo']} <br>
